cfcad6d442a0f29d3b6023041a8ae72f1b10f992
NewAnton <anton1korobov1@gmail.com>
2022-12-17 13:46:46 +0300

    init: basic setup

---
+++ frontEnd/src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './Header.scss';
+
+export function Header() {
+  return <h1>Header</h1>;
+}
---
+++ frontEnd/src/pages/Page404/Page404.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './Page404.scss';
+
+export function Page404() {
+  return <h1>Page404</h1>;
+}
