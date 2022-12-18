ad1ecdb8f50ae8b47d071c1230f54545708b2e11
zoxal <zzoxall@gmail.com>
2022-12-18 12:44:29 +0300

    logo animation on hover

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import './App.scss';
+
 import { Header } from 'components/Header/Header';
 import { Main } from 'pages/Main/Main';
 import { Footer } from 'components/Footer/Footer';
 import { Page404 } from 'pages/Page404/Page404';
 
-import './App.scss';
 import { About } from 'pages/About/About';
 
 export function App() {
