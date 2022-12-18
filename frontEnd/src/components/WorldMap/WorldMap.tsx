6a7dbab47f45b6b5a7d5937b810083e92e3f481b
NewAnton <anton1korobov1@gmail.com>
2022-12-18 14:38:14 +0300

    feat: markers on map

--- frontEnd/src/components/WorldMap/WorldMap.tsx
+++ frontEnd/src/components/WorldMap/WorldMap.tsx
@@ -1,84 +1,84 @@
-// import React, { useState } from 'react';
-// import react from 'react';
-// import Map, { Marker, Popup, GeolocateControl } from 'react-map-gl';
+import React, { useState } from 'react';
+import react from 'react';
+import Map, { Marker, Popup, GeolocateControl } from 'react-map-gl';
 
-// import './WorldMap.scss';
+import './WorldMap.scss';
 
-// import Pin from './WorldMapPin';
+import Pin from './WorldMapPin';
 
-// import { useGetLocationsQuery } from 'services/backend';
-// import { Loading } from 'components/Loading/Loading';
+import { useGetLocationsQuery } from 'services/backend';
+import { Loading } from 'components/Loading/Loading';
 
-// const TOKEN =
-//   'pk.eyJ1Ijoiem94YWwiLCJhIjoiY2xicnI4Z25zMGptNjNvbnRqbmY1cHRvdyJ9.GHmqXKeWVadi-Bq0dEowCQ'; // Set your mapbox token here
+const TOKEN =
+  'pk.eyJ1Ijoiem94YWwiLCJhIjoiY2xicnI4Z25zMGptNjNvbnRqbmY1cHRvdyJ9.GHmqXKeWVadi-Bq0dEowCQ'; // Set your mapbox token here
 
-// // interface popupInterface {
-// //   city: string;
-// //   population: string;
-// //   image: string;
-// //   state: string;
-// //   latitude: number;
-// //   longitude: number;
-// // }
-
-// interface GlobalMapInterface {
-//   setIsReviewsOpen: (value: boolean) => void;
+// interface popupInterface {
+//   city: string;
+//   population: string;
+//   image: string;
+//   state: string;
+//   latitude: number;
+//   longitude: number;
 // }
 
-// export function WorldMap({ setIsReviewsOpen }: GlobalMapInterface) {
-//   // const [popupInfo, setPopupInfo] = useState<popupInterface | null>(null);
-//   const { isLoading, data } = useGetLocationsQuery(); //update after creating review
+interface GlobalMapInterface {
+  setIsReviewsOpen: (value: boolean) => void;
+}
 
-//   const pins = react.useMemo(
-//     () =>
-//       data?.map((location) => (
-//         <Marker
-//           key={location.id}
-//           longitude={Number(location.coordinates.longitude)}
-//           latitude={Number(location.coordinates.latitude)}
-//           anchor="bottom"
-//           onClick={(e) => {
-//             // If we let the click event propagates to the map, it will immediately close the popup
-//             // with `closeOnClick: true`
-//             e.originalEvent.stopPropagation();
-//             setIsReviewsOpen(true);
-//           }}
-//         >
-//           <Pin />
-//         </Marker>
-//       )),
-//     []
-//   );
-//   return (
-//     <Map
-//       initialViewState={{
-//         latitude: 40,
-//         longitude: -100,
-//         zoom: 3.5,
-//         bearing: 0,
-//         pitch: 0,
-//         // projection: 'globe', //doesn't work
-//       }}
-//       style={{ width: '100%', height: 400 }}
-//       mapStyle="mapbox://styles/mapbox/light-v11"
-//       mapboxAccessToken={TOKEN}
-//     >
-//       <GeolocateControl position="top-left" />
+export function WorldMap({ setIsReviewsOpen }: GlobalMapInterface) {
+  // const [popupInfo, setPopupInfo] = useState<popupInterface | null>(null);
+  const { isLoading, data } = useGetLocationsQuery(); //update after creating review
 
-//       {isLoading && <Loading />}
-//       {pins}
+  const pins = react.useMemo(
+    () =>
+      data?.map((location) => (
+        <Marker
+          key={location.id}
+          longitude={Number(location.coordinates.longitude)}
+          latitude={Number(location.coordinates.latitude)}
+          anchor="bottom"
+          onClick={(e) => {
+            // If we let the click event propagates to the map, it will immediately close the popup
+            // with `closeOnClick: true`
+            e.originalEvent.stopPropagation();
+            setIsReviewsOpen(true);
+          }}
+        >
+          <Pin />
+        </Marker>
+      )),
+    [data]
+  );
+  return (
+    <Map
+      initialViewState={{
+        latitude: 40,
+        longitude: -100,
+        zoom: 3.5,
+        bearing: 0,
+        pitch: 0,
+        // projection: 'globe', //doesn't work
+      }}
+      style={{ width: '100%', height: 400 }}
+      mapStyle="mapbox://styles/mapbox/light-v11"
+      mapboxAccessToken={TOKEN}
+    >
+      <GeolocateControl position="top-left" />
 
-//       {/* {popupInfo && (
-//         <Popup
-//           anchor="top"
-//           longitude={Number(popupInfo.longitude)}
-//           latitude={Number(popupInfo.latitude)}
-//           onClose={() => setPopupInfo(null)}
-//         >
-//           <div className="map-popup">{popupInfo.city}</div>
-//           <img width="100%" src={popupInfo.image} />
-//         </Popup>
-//       )} */}
-//     </Map>
-//   );
-// }
+      {isLoading && <Loading />}
+      {pins}
+
+      {/* {popupInfo && (
+        <Popup
+          anchor="top"
+          longitude={Number(popupInfo.longitude)}
+          latitude={Number(popupInfo.latitude)}
+          onClose={() => setPopupInfo(null)}
+        >
+          <div className="map-popup">{popupInfo.city}</div>
+          <img width="100%" src={popupInfo.image} />
+        </Popup>
+      )} */}
+    </Map>
+  );
+}
