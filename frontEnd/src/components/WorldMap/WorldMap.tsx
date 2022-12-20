1503295db14a60ad0257261679df532b6549211f
NewAnton <anton1korobov1@gmail.com>
2022-12-20 20:52:00 +0300

    refactor: map css

--- frontEnd/src/components/WorldMap/WorldMap.tsx
+++ frontEnd/src/components/WorldMap/WorldMap.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import react from 'react';
-import Map, { Marker, Popup, GeolocateControl } from 'react-map-gl';
-
-import './WorldMap.scss';
-
-import Pin from './WorldMapPin';
+import React, { useMemo } from 'react';
+import Map, { Marker } from 'react-map-gl';
 
 import { useGetLocationsQuery } from 'services/backend';
 import { useActions } from 'hooks/useActions';
+import Pin from './WorldMapPin';
+
+import './WorldMap.scss';
 
 const TOKEN =
   'pk.eyJ1IjoibmV3YW50b24iLCJhIjoiY2xidnl2OHlrMHJ2eTN3bXNteGN6a2MyYSJ9.stAVYrO5EP2Xu89LUrgUHA';
@@ -18,10 +16,9 @@ interface GlobalMapInterface {
 
 export function WorldMap({ setIsReviewsOpen }: GlobalMapInterface) {
   const { setLocationId } = useActions();
-  // const [popupInfo, setPopupInfo] = useState<popupInterface | null>(null);
   const { data } = useGetLocationsQuery();
 
-  const pins = react.useMemo(
+  const pins = useMemo(
     () =>
       data?.map((location) => (
         <Marker
@@ -50,27 +47,13 @@ export function WorldMap({ setIsReviewsOpen }: GlobalMapInterface) {
           bearing: 0,
           pitch: 0,
         }}
-        style={{ width: '100%', height: '100%' }}
-        mapStyle="mapbox://styles/newanton/clbwa240n008014o9alqq0tt7"
-        // mapStyle="mapbox://styles/mapbox/light-v11"
         mapboxAccessToken={TOKEN}
+        style={{ width: '100%', height: '100%' }}
+        // mapStyle="mapbox://styles/newanton/clbwa240n008014o9alqq0tt7"
+        mapStyle="mapbox://styles/mapbox/light-v11"
         // projection={'globe'}
       >
-        <GeolocateControl position="top-left" />
-
         {pins}
-
-        {/* {popupInfo && (
-        <Popup
-          anchor="top"
-          longitude={Number(popupInfo.longitude)}
-          latitude={Number(popupInfo.latitude)}
-          onClose={() => setPopupInfo(null)}
-        >
-          <div className="map-popup">{popupInfo.city}</div>
-          <img width="100%" src={popupInfo.image} />
-        </Popup>
-      )} */}
       </Map>
     </div>
   );
