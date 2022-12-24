cf5fe2a7b1713aa3b90a741a15708ad714f96f85
NewAnton <anton1korobov1@gmail.com>
2022-12-24 19:01:10 +0300

    feat: rewrite health check

--- backEnd/src/health/health.controller.ts
+++ backEnd/src/health/health.controller.ts
@@ -1,21 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-import {
-  HealthCheckService,
-  HttpHealthIndicator,
-  HealthCheck,
-} from '@nestjs/terminus';
+import { HealthCheckService, HealthCheck } from '@nestjs/terminus';
 
 @Controller('health')
 export class HealthController {
-  constructor(
-    private health: HealthCheckService,
-    private http: HttpHealthIndicator,
-    private healthCheckService: HealthCheckService,
-  ) {}
+  constructor(private health: HealthCheckService) {}
 
   @Get()
   @HealthCheck()
   check() {
-    return this.healthCheckService.check([]);
+    return this.health.check([]);
   }
 }
